001e099a3408af222aaedbf5b7d7add8d553cd98
subhamku2020 <71179793+subhamku2020@users.noreply.github.com>
2025-11-01 19:17:52 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCn8R7_sqLIouUmAZQK98t40t4ZpkXa_6Y",
+  authDomain: "investment-tracker-rrt0im.firebaseapp.com",
+  projectId: "investment-tracker-rrt0im",
+  storageBucket: "investment-tracker-rrt0im.firebasestorage.app",
+  messagingSenderId: "782669695072",
+  appId: "1:782669695072:web:fe51903382a225fa941b05"
+};
